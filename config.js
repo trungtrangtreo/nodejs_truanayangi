888bd53ffd522656c83f14fdf98f4849541bf9bf
trunglv <trunglv@namviet-corp.com.vn>
2018-06-07 11:27:52 +0700

    fsfs

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	'secret': 'ilovescotchyscotch',
+	'database': 'mongodb://trunglv:trung123456@ds247690.mlab.com:47690/shop',
+	'server_key' : 'AAAA4P1Usog:APA91bGOl6-UgwJr17Oe9vXtuEFLJQiTp61Gpmd_1-fMgCW4kXfyh_i_XHxzh0wDDAWsJMtalLMYPJZxAWID-FzIWaLqAqh0_4F9-QuKZgAZQIa1a9cGpwpAScf4lDtTNAJcMQhEsd_i'
+
+};
